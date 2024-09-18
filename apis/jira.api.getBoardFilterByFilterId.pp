ea7701989f89940e192415edd0af0c3febaf2cf2
favdv <50656637+favdv@users.noreply.github.com>
2024-09-18 17:07:47 +0100

    Update jira.api.getBoardFilterByFilterId.pp

--- apis/jira.api.getBoardFilterByFilterId.pp
+++ apis/jira.api.getBoardFilterByFilterId.pp
@@ -2,7 +2,7 @@ let
 functionName = "Jira.GetFilterByFilterId",
 doc = 
   [
-    Documentation.Api = "https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-filters/#api-rest-api-2-filter-id-get",
+    Documentation.Api = "https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-filters/#api-rest-api-3-filter-id-get",
     Documentation.Name =  "Get JIRA Filter by Filter Id", 
     Documentation.Description = "This function is a jira specific pattern, which retrieves the filter for specific filter ids. Ensure you modify your domain(s) to suit your specific requirements. Note that the function name is dependent on the name of the query in PowerBI in which the function is pasted, so if the Query is called 'Query1', replace "&functionName&" with Query1 when calling the function.", 
     Documentation.Examples = {
@@ -26,7 +26,7 @@ let NewFilterColumnName = if NewFilterColumnName = null then "jql" else NewFilte
 	      then 
 		    Json.Document(Web.Contents(
 	          "<domain>",
-	          [RelativePath = "/rest/api/2/filter/" &Text.From( Record.Field(_,FilterIds))]
+	          [RelativePath = "/rest/api/3/filter/" &Text.From( Record.Field(_,FilterIds))]
 	        )) 
 	      else null
 	    ), NewFilterColumnName, {"jql"}, {NewFilterColumnName}
